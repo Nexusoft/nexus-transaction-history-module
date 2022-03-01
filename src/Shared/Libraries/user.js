b37b5e85aeb0e409c41be44b773775e625932d74
Kendal Cormany <kendal.cormany@nexus.io>
2022-03-01 10:53:17 -0700

    refactored some  of user.js

--- src/Shared/Libraries/user.js
+++ src/Shared/Libraries/user.js
@@ -26,6 +26,16 @@ export const GetAccountTransactions = (accounts, limit) => async (dispatch) => {
     if (accounts) {
       await asyncForEach(accounts, async (account) => {
         if (account.token === '0') {
+          /*
+          console.log(account);
+          const result = await apiCall('users/list/transactions', {
+            verbose: 'summary',
+            address: account.address,
+            where: `results.contracts.address=${account.address}`,
+            limit: limit || 100,
+          });
+          console.log(result);
+          */
           const result = await apiCall('finance/transactions/all', {
             address: account.address,
             limit: limit || 100,
@@ -41,10 +51,29 @@ export const GetAccountTransactions = (accounts, limit) => async (dispatch) => {
           });
         }
       });
-      const asdf = await getLegacyTransactions(transactions);
+      let legacyAddressTransactions = [];
+      try {
+        legacyAddressTransactions = await getLegacyTransactions(transactions);
+      } catch (error) {}
+      const legacyTransactions = await apiCall('users/list/transactions', {
+        verbose: 'summary',
+        where: `results.contracts.OP=LEGACY`,
+        limit: limit || 100,
+      });
+      if (legacyTransactions) {
+        legacyTransactions.forEach((element) => {
+          const temp2 = element.contracts
+            .map((e) => {
+              return { ...e, txid: element.txid };
+            })
+            .filter((e) => e.OP != 'CREATE');
+
+          transactions = transactions.concat(temp2);
+        });
+      }
       dispatch({
         type: TYPE.SET_ACCOUNT_TRANSACTIONS,
-        payload: transactions.concat(asdf),
+        payload: transactions.concat(legacyAddressTransactions),
       });
     }
   } catch (error) {
